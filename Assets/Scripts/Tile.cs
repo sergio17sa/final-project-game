b9d932d6acdc06d09b5b1b940fd76d3b66d30f7b
Andres Lara <andres-lh@outlook.com>
2022-12-11 12:45:11 -0500

    feat: add tile behaviour

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -2,19 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class Tile : MonoBehaviour
 {
+    public Unit _unit;
+
+    [SerializeField] private bool _isWalkable;
     [SerializeField] private GameObject _hightlight;
+    [field: SerializeField] public GameObject RangeHighlight { get; private set; }
+    public bool IsInRange { get; set; } = false;
+    public bool IsCheck { get; set; } = false;
+    public int Visited { get; set; } = -1;
+    public Tile Parent { get; set; } = null;
+
+    //change later
 
 
     private void OnMouseEnter()
     {
-        _hightlight?.SetActive(true);
+        _hightlight.SetActive(true);
 
     }
 
     private void OnMouseExit()
     {
-        _hightlight?.SetActive(false);
+        _hightlight.SetActive(false);
     }
+
+    public bool Walkable => _isWalkable && _unit == null;
+
+    public void SetUnit(Unit unit)
+    {
+        if(_unit != null) return;
+        unit.transform.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
+        _unit = unit;
+    }
+
+    //Move later
+    private void OnMouseDown()
+    {
+        ActionManager.Instance.MouseDown(this);
+    }
+
+    
 }
