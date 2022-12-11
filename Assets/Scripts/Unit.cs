c29c8f3796d540fbfba5bba22b564660bfcb61a1
Andres Lara <andres-lh@outlook.com>
2022-12-11 12:46:10 -0500

    feat: base unit script

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    /// <summary>
+    /// Tile on which the unit is on
+    /// </summary>
+    private Tile _unitTile;
+
+
+}
