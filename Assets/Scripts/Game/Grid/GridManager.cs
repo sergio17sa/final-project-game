74bbc6acb86b2cf669141a0d81568afb294b3af2
Andres-lh <andres-lh@outlook.com>
2022-12-31 15:40:45 -0500

    feat: add size setup fields

--- Assets/Scripts/Game/Grid/GridManager.cs
+++ Assets/Scripts/Game/Grid/GridManager.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class GridManager : Singleton<GridManager>
 {
     [SerializeField] private Transform _tileDebug;
+    [SerializeField] private int _cellSize;
+    [SerializeField] private int _gridHeight;
+    [SerializeField] private int _gridWidth;
     private GridSystem _gridSystem;
     private void Start() 
     {
         
-        _gridSystem = new GridSystem(10, 10, 2);
+        _gridSystem = new GridSystem(_gridWidth, _gridHeight, _cellSize);
         
         _gridSystem.CreateDebugTiles(_tileDebug);
     }
