d44034d082b1478c78c022d2665079104e9f622d
Sergio Salgado <96848977+sergio17sa@users.noreply.github.com>
2023-01-02 21:14:34 -0500

    merge with development

---
+++ Assets/Scripts/Multiplayer/NetworkPlayer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class NetworkPlayer : NetworkBehaviour
+{
+    private void Update()
+    {
+        if(!IsOwner) return;
+        
+        Vector3 moveDir = new Vector3(0, 0, 0);
+
+        if (Input.GetKey(KeyCode.W)) moveDir.z += 1f;
+        if (Input.GetKey(KeyCode.S)) moveDir.z += -1f;
+        if (Input.GetKey(KeyCode.D)) moveDir.x += 1f;
+        if (Input.GetKey(KeyCode.A)) moveDir.x += -1f;
+
+        float speed = 10f;
+        transform.Translate(moveDir * speed * Time.deltaTime);
+    }
+
+}
