56f055cb48d36963c21e30beaba6c2dd0d03a309
Andres-lh <andres-lh@outlook.com>
2023-01-05 13:22:08 -0500

    feat: apply pathfinding to move action

--- Assets/Scripts/Game/PathFinding/PathFinding.cs
+++ Assets/Scripts/Game/PathFinding/PathFinding.cs
@@ -1,30 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.U2D.Path.GUIFramework;
 using UnityEngine;
 
 public class PathFinding : Singleton<PathFinding>
 {
     private const int MOVE_STRAIGHT_COST = 10;
-    private const int MOVE_DIAGONAL_COST = 14;
 
     private GridSystem<PathNode> _gridSystem;
+    [SerializeField] private LayerMask obstaclesLayerMask;
 
     [SerializeField] Transform _debugTile;
-
-    protected override void Awake()
-    {
-        base.Awake();
-
-        
-    }
 
     public void Setup(int width, int height, float cellSize)
     {
         _gridSystem = new GridSystem<PathNode>(width, height, cellSize,
             (GridSystem<PathNode> g, TilePosition tilePosition) => new PathNode(tilePosition));
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int z = 0; z < height; z++)
+            {
+                TilePosition tilePosition = new TilePosition(x, z);
+                Vector3 worldPosition = GridManager.Instance.GetWorldPosition(tilePosition);
+                float raycastOffsetDistance = 5f;
+                if (Physics.Raycast(
+                    worldPosition + Vector3.down * raycastOffsetDistance,
+                    Vector3.up,
+                    raycastOffsetDistance * 2,
+                    obstaclesLayerMask))
+                {
+                    GetNode(x, z).IsWalkable = false;
+                }
+            }
+        }
+
+        _gridSystem.CreateDebugTiles(_debugTile);
     }
 
-    public List<TilePosition> FindPath(TilePosition startTilePosition, TilePosition endTilePosition)
+    public List<TilePosition> FindPath(TilePosition startTilePosition, TilePosition endTilePosition, out int pathLength)
     {
         List<PathNode> openList = new List<PathNode>();
         List<PathNode> closedList = new List<PathNode>();
@@ -43,7 +57,7 @@ public class PathFinding : Singleton<PathFinding>
 
                 pathNode.GCost = int.MaxValue;
                 pathNode.HCost = 0;
-                pathNode.CalculteFCost();
+                pathNode.CalculateFCost();
 
                 pathNode.ResetLastNode();
             }
@@ -51,7 +65,7 @@ public class PathFinding : Singleton<PathFinding>
 
         startNode.GCost = 0;
         startNode.HCost = CalculateDistance(startTilePosition, endTilePosition);
-        startNode.CalculteFCost();
+        startNode.CalculateFCost();
 
         while (openList.Count > 0)
         {
@@ -59,25 +73,32 @@ public class PathFinding : Singleton<PathFinding>
 
             if (currentNode == endNode)
             {
+                pathLength = endNode.FCost;
                 return CalculatePath(endNode);
             }
 
             openList.Remove(currentNode);
             closedList.Add(currentNode);
 
-            foreach(PathNode neighbourNode in GetNeighbours(currentNode))
+            foreach (PathNode neighbourNode in GetNeighbours(currentNode))
             {
                 if (closedList.Contains(neighbourNode)) continue;
 
-                int tentativeGCost = 
+                if (!neighbourNode.IsWalkable)
+                {
+                    closedList.Add(neighbourNode);
+                    continue;
+                }
+
+                int tentativeGCost =
                     currentNode.GCost + CalculateDistance(currentNode.NodeTilePosition, neighbourNode.NodeTilePosition);
 
-                if(tentativeGCost < neighbourNode.GCost)
+                if (tentativeGCost < neighbourNode.GCost)
                 {
                     neighbourNode.LastNode = currentNode;
                     neighbourNode.GCost = tentativeGCost;
                     neighbourNode.HCost = CalculateDistance(neighbourNode.NodeTilePosition, endTilePosition);
-                    neighbourNode.CalculteFCost();
+                    neighbourNode.CalculateFCost();
 
                     if (!openList.Contains(neighbourNode)) openList.Add(neighbourNode);
                 }
@@ -86,25 +107,15 @@ public class PathFinding : Singleton<PathFinding>
         }
 
         //No path found
+        pathLength = 0;
         return null;
     }
-
-    /*public int CalculateDistance(TilePosition tilePositionA, TilePosition tilePositionB)
-    {
-        TilePosition tilePositionDistance = tilePositionA - tilePositionB;
-        int distance = Mathf.Abs(tilePositionDistance.x) + Mathf.Abs(tilePositionDistance.z);
-        return distance * MOVE_STRAIGHT_COST;
-    }*/
 
     public int CalculateDistance(TilePosition tilePositionA, TilePosition tilePositionB)
     {
         TilePosition tilePositionDistance = tilePositionA - tilePositionB;
-
-        int xDistance = Mathf.Abs(tilePositionDistance.x);
-        int zDistance = Mathf.Abs(tilePositionDistance.z);
-        int remaining = Mathf.Abs(xDistance - zDistance);
-
-        return MOVE_DIAGONAL_COST * Mathf.Min(xDistance, zDistance) + MOVE_STRAIGHT_COST * remaining;
+        int distance = Mathf.Abs(tilePositionDistance.x) + Mathf.Abs(tilePositionDistance.z);
+        return distance * MOVE_STRAIGHT_COST;
     }
 
     private PathNode GetlowstFCostNode(List<PathNode> pathNodes)
@@ -132,45 +143,21 @@ public class PathFinding : Singleton<PathFinding>
         {
             //left node
             neighbours.Add(GetNode(tilePosition.x - 1, tilePosition.z));
-
-            if (tilePosition.z - 1 >= 0)
-            {
-                //left down
-                neighbours.Add(GetNode(tilePosition.x - 1, tilePosition.z - 1));
-            }
-
-            if (tilePosition.z + 1 < _gridSystem.GetHeight())
-            {
-                //left up
-                neighbours.Add(GetNode(tilePosition.x - 1, tilePosition.z + 1));
-            }
         }
 
         if (tilePosition.x + 1 < _gridSystem.GetWidth())
         {
             //right node
             neighbours.Add(GetNode(tilePosition.x + 1, tilePosition.z));
-
-            if (tilePosition.z - 1 >= 0)
-            {
-                //right down
-                neighbours.Add(GetNode(tilePosition.x + 1, tilePosition.z - 1));
-            }
-
-            if (tilePosition.z + 1 < _gridSystem.GetHeight())
-            {
-                //right up
-                neighbours.Add(GetNode(tilePosition.x + 1, tilePosition.z + 1));
-            }
         }
 
-        if(tilePosition.z -1 >= 0)
+        if (tilePosition.z - 1 >= 0)
         {
             //down node
             neighbours.Add(GetNode(tilePosition.x, tilePosition.z - 1));
         }
-        
-        if(tilePosition.z + 1 < _gridSystem.GetHeight())
+
+        if (tilePosition.z + 1 < _gridSystem.GetHeight())
         {
             //up node
             neighbours.Add(GetNode(tilePosition.x, tilePosition.z + 1));
@@ -187,7 +174,7 @@ public class PathFinding : Singleton<PathFinding>
 
         PathNode currentNode = endNode;
 
-        while(currentNode.LastNode != null)
+        while (currentNode.LastNode != null)
         {
             pathNodes.Add(currentNode.LastNode);
             currentNode = currentNode.LastNode;
@@ -197,7 +184,7 @@ public class PathFinding : Singleton<PathFinding>
 
         List<TilePosition> nodesTilePosition = new List<TilePosition>();
 
-        foreach(PathNode pathNode in pathNodes)
+        foreach (PathNode pathNode in pathNodes)
         {
             nodesTilePosition.Add(pathNode.NodeTilePosition);
         }
@@ -208,4 +195,16 @@ public class PathFinding : Singleton<PathFinding>
 
     private PathNode GetNode(int x, int z) => _gridSystem.GetTile(new TilePosition(x, z));
 
+    public bool HasPath(TilePosition startTilePosition, TilePosition endTilePosition)
+    {
+        return FindPath(startTilePosition, endTilePosition, out int pathLength) != null;
+    }
+
+    public int GetPathLength(TilePosition startTilePosition, TilePosition endTilePosition)
+    {
+        FindPath(startTilePosition, endTilePosition, out int pathLength);
+        return pathLength;
+    }
+
+    public bool IsWalkableTile(TilePosition tilePosition) => _gridSystem.GetTile(tilePosition).IsWalkable;
 }
