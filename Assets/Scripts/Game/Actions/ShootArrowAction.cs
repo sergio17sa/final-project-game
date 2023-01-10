aa51f78de612269c58feed7719f2ee4ef06a18e4
Andres-lh <andres-lh@outlook.com>
2023-01-10 01:07:30 -0500

    feat: create a state machine to sync the animation

--- Assets/Scripts/Game/Actions/ShootArrowAction.cs
+++ Assets/Scripts/Game/Actions/ShootArrowAction.cs
@@ -2,18 +2,103 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.TextCore.Text;
 
 public class ShootArrowAction : BaseAction
 {
     [SerializeField] private int _maxArrowRange = 4;
+    [SerializeField] private LayerMask obstaclesLayerMask;
+
     private Character _targetCharacter;
 
-    [SerializeField] private LayerMask obstaclesLayerMask;
+    private enum State
+    {
+        Aiming,
+        Shooting,
+        Load,
+        Rest
+    }
+
+    //States duration
+    private float _aimDuration = 0.5f;
+    private float _loadDuration = 2.5f;
+    private float _shootDuration = 0.5f;
+    private float _restDuration = 0.5f;
+
+    //State machine varibles
+    private State state;
+    private float _stateTimer;
+    private bool _canShoot;
+    private bool _canLoad;
 
     private void Update()
     {
         if (!_isActive) return;
+
+        UpdateStateMachine();
+    }
+
+    private void UpdateStateMachine()
+    {
+        _stateTimer -= Time.deltaTime;
+
+        switch (state)
+        {
+            case State.Aiming:
+                Aim();
+                if (_stateTimer <= 0) TransitionToReloadState();
+                break;
+            case State.Load:
+                if(_canLoad) LoadBow();
+                if (_stateTimer <= 0) TransitionToShootingState();
+                break;
+            case State.Shooting:
+                if (_canShoot) Shoot();
+                if (_stateTimer <= 0) TransitionToRestState();
+                break;
+            case State.Rest:
+                if (_stateTimer <= 0) ActionComplete(this);
+                break;
+        }
+    }
+
+    private void Aim()
+    {
+        Vector3 aimDir = (_targetCharacter.transform.position - _character.transform.position).normalized;
+
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Lerp(transform.forward, aimDir, Time.deltaTime * rotateSpeed);
+    }
+
+    private void LoadBow()
+    {
+        //Trigger animation
+        _character.GetAttack();
+        _canLoad = false;
+    }
+
+    private void TransitionToReloadState()
+    {
+        state = State.Load;
+        _stateTimer = _loadDuration;
+    }
+
+    private void TransitionToShootingState()
+    {
+        state = State.Shooting;
+        _stateTimer = _shootDuration;
+    }
+
+    private void TransitionToRestState()
+    {
+        state = State.Rest;
+        _stateTimer = _restDuration;
+    }
+
+    private void Shoot()
+    {
+        _targetCharacter.GetDamage(50);
+
+        _canShoot = false;
     }
     public override string GetActionName()
     {
@@ -75,17 +160,13 @@ public class ShootArrowAction : BaseAction
     {
         _targetCharacter = GridManager.Instance.GetCharacterAtTilePosition(tilePosition);
 
-        /*Vector3 aimDir = (_targetCharacter.transform.position - _character.transform.position).normalized;
+        state = State.Aiming;
+        _stateTimer = _aimDuration;
 
-        float rotateSpeed = 10f;
-        transform.forward = Vector3.Lerp(transform.forward, aimDir, Time.deltaTime * rotateSpeed);*/
+        _canShoot = true;
+        _canLoad = true;
 
         ActionStart(onActionComplete);
-
-        _character.GetAttack();
-        _targetCharacter.GetDamage(50);
-
-        ActionComplete(this);
     }
 
     public int GetArrowRange() => _maxArrowRange;
