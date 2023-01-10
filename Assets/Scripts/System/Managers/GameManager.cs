3ea8c7c54646879727d909f75868daf9d69bc2f6
Oscar Alarcon <oscaralarcon9906@gmail.com>
2023-01-10 01:08:59 -0500

    Revert "Update GameManager.cs"
    This reverts commit cf7095457bd0f9067e243cfc57d420f5efb5c65b.

--- Assets/Scripts/System/Managers/GameManager.cs
+++ Assets/Scripts/System/Managers/GameManager.cs
@@ -2,26 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class GameManager : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
     [Header("Setup")]
     public bool isActive;
     public List<GameObject> players = new List<GameObject>();
-
-    public static GameManager Instance;
-
-    private void Awake()
-    {
-        if (Instance != null)
-        {
-            Destroy(this);
-            return;
-        }
-
-        Instance = this;
-        DontDestroyOnLoad(this);
-    }
-
 
     public void AddPlayer(GameObject newPlayer)
     {
