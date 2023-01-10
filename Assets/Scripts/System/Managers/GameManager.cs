cf7095457bd0f9067e243cfc57d420f5efb5c65b
Oscar Alarcon <oscaralarcon9906@gmail.com>
2023-01-09 20:04:16 -0500

    Update GameManager.cs

--- Assets/Scripts/System/Managers/GameManager.cs
+++ Assets/Scripts/System/Managers/GameManager.cs
@@ -2,11 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class GameManager : Singleton<GameManager>
+public class GameManager : MonoBehaviour
 {
     [Header("Setup")]
     public bool isActive;
     public List<GameObject> players = new List<GameObject>();
+
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(this);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(this);
+    }
+
 
     public void AddPlayer(GameObject newPlayer)
     {
