c1f4257476a69fc47018513f33b264802dcab855
Andres-lh <andres-lh@outlook.com>
2023-01-18 00:48:33 -0500

    feat: use logos to represent actions

--- Assets/Scripts/Game/UI/ActionButtonUI.cs
+++ Assets/Scripts/Game/UI/ActionButtonUI.cs
@@ -3,20 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
-using UnityEngine.TextCore.Text;
 
 public class ActionButtonUI : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI _buttonText;
     [SerializeField] private Button _button;
+    [SerializeField] private TextMeshProUGUI _buttonText;
     [SerializeField] private Image _buttonImage;
+    [SerializeField] private Sprite _attackSprite;
+    [SerializeField] private Sprite _moveSprite;
 
     private BaseAction _baseAction;
 
     public void SetButtonAction(BaseAction baseAction)
     {
         _baseAction = baseAction;
+
         _buttonText.text = baseAction.GetActionName().ToUpper();
+        _buttonImage.sprite = baseAction.GetActionName().ToUpper() == "MOVE" ? _moveSprite : _attackSprite;
+
         _button.onClick.AddListener(() =>
         {
             CharacterActionManager.Instance.SetSelectedAction(baseAction);
@@ -29,7 +33,7 @@ public class ActionButtonUI : MonoBehaviour
 
         if (selectedBaseAction == _baseAction)
         {
-            _buttonImage.color = Color.green;
+            _buttonImage.color = Color.cyan;
         }
         else
         {
